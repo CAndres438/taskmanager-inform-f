5cd6245e0d86b58f728d105bb167da8574afb3b5
CAndres438 <candresortiz123@gmail.com>
2025-05-16 23:42:30 -0500

    🔧 build: configure Vite and polyfills to support Websocket

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './i18n'
+import './i18n';
 
+//Polyfill for SockJS + Stomp
+import process from 'process';
+import { Buffer } from 'buffer';
+
+window.global = window;
+window.process = process;
+window.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
